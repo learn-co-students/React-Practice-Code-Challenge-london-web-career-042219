6393b6895bbe6cc4082f1871e0a37e30f1c9f8fd
LeahB8 <leahboyd08@gmail.com>
2019-07-01 16:05:09 +0100

    Done.

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,50 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+class Sushi extends React.Component {
+  constructor(props) {
+    super(props);
+    if (props.eatenSushi.includes(props.sushi.id)) {
+      this.state = {
+        eaten: true
+      };
+    } else {
+      this.state = {
+        eaten: false
+      };
+    }
+  }
+
+  setSushiToEaten = () => {
+    if (this.state.eaten) {
+      alert("You've already eaten this sushi");
+    } else if (this.props.money < this.props.sushi.price) {
+      alert("You don't have enough money");
+    } else {
+      this.props.eatSushi(this.props.sushi);
+      this.setState({ eaten: true });
+    }
+  };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.sushi.id !== this.props.sushi.id) {
+      this.setState({ eaten: false });
+    }
+  }
+
+  render() {
+    const { id, name, img_url, price } = this.props.sushi;
+
+    return (
+      <div className="sushi">
+        <div className="plate" onClick={this.setSushiToEaten}>
+          {this.state.eaten ? null : <img src={img_url} width="100%" />}
+        </div>
+        <h4 className="sushi-details">
+          {name} - ${price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
+    );
+  }
 }
 
-export default Sushi
+export default Sushi;
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,38 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
+
+class SushiContainer extends React.Component {
+  state = {
+    displayIndex: 0,
+  }
+
+  renderMore = (event) => {
+    event.persist()
+    if (this.state.displayIndex === 96) {
+      this.setState({ displayIndex: 0})
+    } else {
+      this.setState({ displayIndex: this.state.displayIndex + 4 })
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          this.props.sushis
+          .slice(this.state.displayIndex, this.state.displayIndex + 4)
+          .map((sushi, idx) => <Sushi key={idx} sushi={sushi} money={this.props.money} eatSushi={this.props.eatSushi} eatenSushi={this.props.eatenSushi}/>
+          )
         }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+          <MoreButton renderMore={this.renderMore} />
+        </div>
+      </Fragment>
+    )
+  }
+
 }
 
 export default SushiContainer
