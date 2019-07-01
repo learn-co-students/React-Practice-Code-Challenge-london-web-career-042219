4729b5d97fc35e4220057c4e0f81460c7d8a6d64
Lauren Carne <oreocookie@hotmail.co.uk>
2019-07-01 14:59:18 +0100

    not finished. had enough

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,72 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sushi: [],
+      eaten: false,
+      eatenArr: [],
+      money: 100,
+      total: [],
+      moneyRemaining: 0
+    };
+  }
+  componentDidMount() {
+    this.fetchMoreDataFromServer(0, 4);
+  }
+
+  fetchDataFromServer = () => {
+    return fetch(API).then(response => response.json());
+  };
+
+  fetchMoreDataFromServer = (num1, num2) => {
+    return this.fetchDataFromServer().then(data =>
+      this.setState({ sushi: data.slice(num1, num2) })
+    );
+  };
+
+  eatenSushi = sushi => {
+    this.setState({
+      total: [...this.state.total, sushi.price]
+    });
+    let array = this.state.sushi.filter(item => item.id !== sushi.id);
+    this.setState({
+      sushi: array
+    });
+
+    this.setState({
+      eatenArr: [...this.state.eatenArr, sushi]
+    });
+  };
+
+  updateTotal = () => {
+    let total = this.state.total.reduce((a, b) => a + b, 0);
+    this.setState({
+      moneyRemaining: this.state.money - total
+    });
+  };
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          moneyRemaining={this.state.moneyRemaining}
+          eatenSushi={this.eatenSushi}
+          fetchMoreDataFromServer={this.fetchMoreDataFromServer}
+          sushi={this.state.sushi}
+          eaten={this.state.eaten}
+        />
+        <Table
+          moneyRemaining={this.state.moneyRemaining}
+          updateTotal={this.updateTotal}
+          eatenArr={this.state.eatenArr}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,32 +1,31 @@
-import React, { Fragment } from 'react'
+import React, { Component } from "react";
 
-const Table = (props) => {
-
-  const renderPlates = (array) => {
-    return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
-    })
+class Table extends Component {
+  constructor() {
+    super();
+    this.state = {
+      eaten: false
+    };
   }
 
-  return (
-    <Fragment>
-      <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
-      </h1>
-      <div className="table">
-        <div className="stack">
-          {
-            /* 
-               renderPlates takes an array 
-               and renders an empty plate
-               for every element in the array
-            */
-            renderPlates([])
-          }
+  renderPlates = array => {
+    return array.map((x, index) => {
+      return <div className="empty-plate" style={{ top: -7 * index }} />;
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="remaining">
+          You have: ${this.props.moneyRemaining} remaining!
+        </h1>
+        <div className="table">
+          <div className="stack">{this.renderPlates(this.props.eatenArr)}</div>
         </div>
       </div>
-    </Fragment>
-  )
+    );
+  }
 }
 
-export default Table
+export default Table;
